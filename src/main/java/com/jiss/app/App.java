08611e761811a42c8a414f402ff6cb280a77c9b2
Jose Ignacio Seco <joseignacio.secosanz@amadeus.com>
2023-11-25 01:35:05 +0100

    Add first classes

--- src/main/java/com/jiss/app/App.java
+++ src/main/java/com/jiss/app/App.java
@@ -1,13 +1,29 @@
 package com.jiss.app;
 
+import com.jiss.app.LanternaFirst;
+import java.io.IOException;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+      System.out.println( "Hello World!" );
+      LanternaFirst myApp = new LanternaFirst();
+
+      try
+      {
+        myApp.testWindow();
+      }
+      catch(IOException e)
+      {
+      }
+      catch(InterruptedException e)
+      {
+      }
     }
+
 }
---
+++ src/main/java/com/jiss/app/LanternaFirst.java
@@ -0,0 +1,37 @@
+package com.jiss.app;
+
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.bundle.LanternaThemes;
+import com.googlecode.lanterna.gui2.BasicWindow;
+import com.googlecode.lanterna.gui2.DefaultWindowManager;
+import com.googlecode.lanterna.gui2.EmptySpace;
+import com.googlecode.lanterna.gui2.Label;
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import java.io.IOException;
+
+public class LanternaFirst
+{
+    public LanternaFirst()
+    {}
+
+    public void testWindow() throws IOException, InterruptedException
+    {
+      Terminal terminal = new DefaultTerminalFactory().createTerminal();
+      Screen screen = new TerminalScreen(terminal);
+      screen.startScreen();
+
+      Label label = new Label("test");
+
+      BasicWindow window = new BasicWindow();
+      window.setComponent(label);
+
+      MultiWindowTextGUI gui = new MultiWindowTextGUI(screen, new DefaultWindowManager(),
+          new EmptySpace(TextColor.ANSI.BLACK));
+      gui.setTheme(LanternaThemes.getRegisteredTheme("businessmachine"));
+      gui.addWindowAndWait(window);
+  }
+}
